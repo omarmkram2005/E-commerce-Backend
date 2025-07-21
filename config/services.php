fe05b79d0932e36be1083663998050540c7c4ef7
omar mkram <mkramwolf@gmail.com>
2025-07-21 04:31:01 +0300

    ddd

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:3000/auth/google/callback',
+        'redirect' => 'https://react-js-e-commercee.netlify.app/auth/google/callback',
     ],
 
 ];
